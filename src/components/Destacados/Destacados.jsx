b5758ab747fe631654a7bf18cc971d58982574d4
JuanMata5 <juanjitus12@gmail.com>
2024-01-04 23:59:11 -0300

    react-changes

--- src/components/Destacados/Destacados.jsx
+++ src/components/Destacados/Destacados.jsx
@@ -6,8 +6,8 @@ function Categoria() {
 
   return (
     <section className="pt-10 flex flex-col gap-2 w-full lg:p-10 xl:p-0 xl:pt-10  sm:pl-5 sm:pr-5 xl:text-left xl:items-baseline xs:justify-center xs:items-center   ">
-    <h1 className="text-5xl font-extrabold">Categorias</h1>
-    <h2 className="text-2xl">Destacadas</h2>
+    <h1 className="text-5xl font-extrabold">Comics</h1>
+    <h2 className="text-2xl">Destacados</h2>
     <div className="flex justify-between flex-wrap gap-10 pt-5 xs:justify-center ">
       {productos.map((product) => (
         <Card
